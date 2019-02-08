c76f9ba2ec11101a2c119994a39190bffdcbba5f
ronan <g00342393@gmit.ie>
2019-02-08 21:08:43 +0000

    Worked on the InputParser

---
+++ src/InputParser.java
@@ -0,0 +1,45 @@
+import Datacenter.Datacenter;
+import Servers.Server;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class InputParser {
+    public static Datacenter parseFile(String filePath) throws IOException {
+        List<Server> servers = new ArrayList<Server>();
+
+        BufferedReader in = new BufferedReader(new FileReader(filePath));
+
+        String[] specs = in.readLine().split(" ");
+        int[] specInts = Arrays.stream(specs).mapToInt(n -> (Integer.parseInt(n))).toArray();
+        int rows, cols, numUnavail, numPools, numServers;
+        rows = specInts[0];
+        cols = specInts[1];
+        numUnavail = specInts[2];
+        numPools = specInts[3];
+        numServers = specInts[3];
+
+        int[][] unavailSlots = new int[numUnavail][];
+
+        for (int i = 0; i < numUnavail; i++) {
+            StringTokenizer tokenizer = new StringTokenizer(in.readLine());
+            unavailSlots[i] = new int[2];
+            for (int j = 0; j < 2; j++) unavailSlots[i][j] = Integer.parseInt(tokenizer.nextToken());
+        }
+
+
+//        for (int i = 0; i < numServers; i++) {
+//            StringTokenizer tokenizer = new StringTokenizer(in.readLine());
+//            unavailSlots[i] = new int[2];
+//            for (int j = 0; j < 2; j++) unavailSlots[i][j] = Integer.parseInt(tokenizer.nextToken());
+//        }
+
+        return null;
+    }
+}
